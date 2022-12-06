565f77d479bae517d4ab2fc09512271c9911e85a
Ziyu Huang <106014099+ziyu-Huang1@users.noreply.github.com>
2022-12-06 15:32:18 -0800

    Add files via upload

---
+++ public/const/errorText.js
@@ -0,0 +1,18 @@
+const errorText={
+    joinInCommunityErrors:{
+        userNameError:"username should be ... long",
+        userBanned:"Username banned",
+        wrongPassword:"Wrong Password",
+        userNotFound:"No such user found",
+        serverError:"Server error",
+        userInactive: "This user is inactive"
+    },
+    joinInCommunityInform:{
+        userExist:"User already exists"
+    },
+    measurePerformanceInfo:{
+        systemUnavaliable: "The system is meauring performance and unavalible for now. Please try agiain later"
+    }
+}
+
+module.exports=errorText;
